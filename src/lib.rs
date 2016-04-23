f2b6b88d552acfebf3752066718702912ef7d933
Lucio Franco <lucio.franco@du.edu>
2016-04-23 11:17:57 -0600

    added gh-pages push abilty to travis

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![crate_name = "ghp"]
+#![doc(html_root_url = "https://luciofran.co/ghp-rs")]
+
 #![deny(warnings)]
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
